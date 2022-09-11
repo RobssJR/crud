909ddf363338c6bf58ad3dc1cbc18c73f870ce33
AquelaPessoa <robson.alex.jr@gmail.com>
2022-09-11 10:45:12 -0300

    First Commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,14 @@
 package org.example;
 
-//import jakarta.persistence.EntityManager;
-//import jakarta.persistence.EntityManagerFactory;
-//import jakarta.persistence.Persistence;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import org.ui.frmPrincipal;
 
 public class App
 {
-    //private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("endereco");
-    //private static EntityManager entityManager = entityManagerFactory.createEntityManager();
+    private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("endereco");
+    private static EntityManager entityManager = entityManagerFactory.createEntityManager();
 
     public static void main( String[] args )
     {
